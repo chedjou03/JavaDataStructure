7ff5b9bf15be1123e8c2f54f75325a0c926e3e53
chedjou03 <chedjou03@yahoo.fr>
2020-05-02 21:25:22 -0500

    initial commit

--- src/ArrayTest.java
+++ src/ArrayTest.java
@@ -12,6 +12,7 @@ public class ArrayTest {
 		intArray[5] = 1;
 		intArray[6] = -22;
 		for(int i = 0 ; i < intArray.length ; i++) {
+			System.out.println(i);
 			System.out.println(intArray[i]);
 			
 		}
